a546acd93556985d8e929ac2241b3825da9e21fb
Gagan6810 <98400800+Gagan6810@users.noreply.github.com>
2022-05-05 11:19:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/CyZGW0m2c/model.json', modelLoaded);
+}
+
+function modelLoaded(){
+    classifier.classify(gotResults);
+}
+
+function gotResults(error, results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+
+    }
+}
